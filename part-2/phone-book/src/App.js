1d00da6d899d2c949ff0b5e435e330a9f27bc909
TramNguyen <tramtram2205@gmail.com>
2021-07-22 01:38:49 +0300

    Exercise 2.15-2.18

--- part-2/phone-book/src/App.js
+++ part-2/phone-book/src/App.js
@@ -2,19 +2,16 @@ import React, { useState, useEffect } from "react";
 import SearchFilter from "./components/SearchFilter";
 import PersonForm from "./components/PersonForm";
 import RenderAllPeople from "./components/RenderAllPeople";
-import axios from "axios";
+import personService from "./services/notes";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState({ name: "", number: "" });
   const [filterResult, setFilterResult] = useState("");
 
   useEffect(() => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => setPersons(response.data))
-  }, [])
-
+    personService.getAll().then((response) => setPersons(response));
+  }, []);
 
   const handleFilter = (event) => {
     const filter = persons.filter((person) =>
@@ -22,22 +19,46 @@ const App = () => {
     );
     setFilterResult(filter);
   };
-  
+
   const handleChange = (event) => {
     setNewPerson({ ...newPerson, [event.target.name]: event.target.value });
   };
 
   const addPerson = (event) => {
     event.preventDefault();
+    const newPersonObject = { name: newPerson.name, number: newPerson.number };
     if (newPerson.name && newPerson.number) {
       const isDuplicate = persons.filter((el) => el.name === newPerson.name);
       if (isDuplicate.length > 0) {
-        alert(`${newPerson.name} is already added to phonebook`);
+        if (
+          window.confirm(
+            `${newPerson.name} is already added to phonebook, replace the old number with the new one?`
+          )
+        ) {
+          personService.update(isDuplicate[0].id, newPersonObject).then((res) => {
+            setPersons(persons.map(person => {
+              if (person.id === isDuplicate[0].id) {
+                return { ...person, number: newPersonObject.number };
+              } else return person;
+            }))
+          });
+        }
         setNewPerson({ name: "", number: "" });
       } else {
-        setPersons([...persons, newPerson]);
-        setNewPerson({ name: "", number: "" });
+        personService.create(newPersonObject).then((returnedPerson) => {
+          setPersons([...persons, returnedPerson]);
+          setNewPerson({ name: "", number: "" });
+        });
       }
+    }
+  };
+  const handleDelete = (id) => {
+    const [nameDeleting] = persons.filter((person) => person.id === id);
+    if (window.confirm(`Delete ${nameDeleting.name}`)) {
+      personService.deleteData(id).then((res) => {
+        const result = persons.filter((person) => person.id !== id);
+        setPersons(result);
+      });
     }
   };
   return (
@@ -52,7 +73,11 @@ const App = () => {
         newPerson={newPerson}
       />
       <h2>Numbers</h2>
-      <RenderAllPeople filterResult={filterResult} persons={persons} />
+      <RenderAllPeople
+        filterResult={filterResult}
+        persons={persons}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
--- part-2/phone-book/src/components/RenderAllPeople.js
+++ part-2/phone-book/src/components/RenderAllPeople.js
@@ -1,19 +1,31 @@
 import React from "react";
+import personService from "../services/notes";
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
 
-function RenderAllPeople({ filterResult, persons }) {
+function RenderAllPeople({ filterResult, persons, handleDelete }) {
+
+ 
+
   return (
     <div>
       <ul>
         {!filterResult
           ? persons.map((person, index) => (
-              <li key={index}>
-                {person.name} {person.number}
-              </li>
+              <div key={index}>
+                <span>
+                  {person.name} {person.number}
+                </span>
+                <button onClick={() => handleDelete(person.id)}>delete</button>
+              </div>
             ))
           : filterResult.map((person, index) => (
-              <li key={index}>
-                {person.name} {person.number}
-              </li>
+              <div key={index}>
+                <span>
+                  {person.name} {person.number}
+                </span>
+                <button onClick={() => handleDelete(person.id)}>delete</button>
+              </div>
             ))}
       </ul>
     </div>
