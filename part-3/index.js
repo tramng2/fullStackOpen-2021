bfa6c2430ad6eb2302ed62683604d6b47a39ab86
TramNguyen <tramtram2205@gmail.com>
2021-07-25 19:14:14 +0300

    Exercises 3.7-3.8.

--- part-3/index.js
+++ part-3/index.js
@@ -1,8 +1,15 @@
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
