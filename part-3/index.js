850f3d19294abbb1b8939f63fa13061fbf083616
TramNguyen <tramtram2205@gmail.com>
2021-07-27 18:55:28 +0300

    Exercise 3.13-3.14. Conect front-end and back-end with GET and POST request.

--- part-3/index.js
+++ part-3/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.json());
@@ -14,79 +17,48 @@ app.use(
 );
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
-  response.status(200).json(persons);
+  Person.find({}).then((person) => {
+    response.json(person);
+  });
 });
 
-app.get("/info", (request, response) => {
-  const amount = persons.length;
-  const date = new Date();
-  const text = `
-  <div> <p>Phone book has infor for ${amount} people </p><p>${date}</p></div>`;
-  response.status(200).send(text);
-});
+// app.get("/info", (request, response) => {
+//   const amount = persons.length;
+//   const date = new Date();
+//   const text = `
+//   <div> <p>Phone book has infor for ${amount} people </p><p>${date}</p></div>`;
+//   response.status(200).send(text);
+// });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.status(200).json(person);
-  } else {
-    response.status(404).end("Not found");
-  }
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
-});
+// app.delete("/api/persons/:id", (request, response) => {
+//   Person.deleteOne(request.params.id).then();
+//   //   persons = persons.filter((person) => person.id !== id);
+//   response.status(204).end();
+// });
 
 app.post("/api/persons", (request, response) => {
-  const person = request.body;
-  const checkDuplicate = persons.find((el) => el.name === person.name);
-  if (!person.name || !person.number) {
-    return response.status(400).json({
-      error: "name or number is missing",
-    });
+  const body = request.body;
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: "name or number is missing" });
   }
-  if (checkDuplicate) {
-    return response.status(400).json({
-      error: "The name already exists in the phonebook",
-    });
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-  const personContent = request.body;
-  personContent.id = Math.floor(Math.random() * 1000);
-
-  persons = persons.concat(personContent);
-  response.json(persons);
+  person.save().then((savePerson) => {
+    response.json(savePerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
