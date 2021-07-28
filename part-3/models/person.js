433da6720283f15dd63d2b02f3ec9be2da90df90
TramNguyen <tramtram2205@gmail.com>
2021-07-28 22:58:45 +0300

    uibuild

--- part-3/models/person.js
+++ part-3/models/person.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url, {
@@ -12,25 +12,25 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true,
   })
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 3 },
   number: { type: String, required: true, minLength: 8 },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
 // const Person = mongoose.model("Person", personSchema);
--- part-3/mongo.js
+++ part-3/mongo.js
@@ -1,45 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
-let name = process.argv[3];
-let number = process.argv[4];
+const password = process.argv[2]
+let name = process.argv[3]
+let number = process.argv[4]
 
-const url = `mongodb+srv://tram22:${password}@cluster0.pbhci.mongodb.net/phone-book?retryWrites=true&w=majority`;
+const url = `mongodb+srv://tram22:${password}@cluster0.pbhci.mongodb.net/phone-book?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
-});
+})
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: name,
   number: number,
-});
+})
 
-person.save().then((result) => {
-  console.log(`added ${name} number ${number} to phonebook`);
-  mongoose.connection.close();
-});
+person.save().then(() => {
+  console.log(`added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
+})
 
 Person.find({}).then((result) => {
   result.forEach((person) => {
-    console.log(person);
-  });
-  mongoose.connection.close();
-});
+    console.log(person)
+  })
+  mongoose.connection.close()
+})
