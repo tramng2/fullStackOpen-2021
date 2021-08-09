461f12f9af09e9a3c4cfd7ecfe5320995c88cc90
TramNguyen <tramtram2205@gmail.com>
2021-08-09 18:00:05 +0300

    Handling login, connect front-end with backend server on part 4.

---
+++ part-5/bloglist-frontend/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import { getAll } from "./services/blogs";
+import loginService from './services/login' 
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      console.log("user",user)
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (exception) {
+      console.log('wrong')
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  console.log(user);
+  return (
+    <div>
+      <h2>blogs</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          {" "}
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
